c547beca6b1100b2e61858f1639050d4c9368e42
root <root@DESKTOP-3RCJHHS.localdomain>
2022-01-07 12:57:41 +0100

    copia de seguretat

--- dist/presentacio.ab10e3a7.js
+++
@@ -1,2 +0,0 @@
-!function e(){var t=new Date,n=t.getHours(),o=t.getMinutes(),c=t.getSeconds(),i="AM";0==n&&(n=12),n>12&&(n-=12,i="PM");var l=(n=n<10?"0"+n:n)+":"+(o=o<10?"0"+o:o)+":"+(c=c<10?"0"+c:c)+" "+i;document.getElementById("MyClockDisplay").innerText=l,document.getElementById("MyClockDisplay").textContent=l,setTimeout(e,1e3)}();
-//# sourceMappingURL=presentacio.ab10e3a7.js.map
