dfcf315afa704b96e8c949fa0edd3ae5697e405d
root <root@DESKTOP-3RCJHHS.localdomain>
2022-01-09 12:45:28 +0100

    copia de seguretat

---
+++ dist/index.da04a2a2.js
@@ -0,0 +1,2 @@
+console.log("Hello ".concat("world"));
+//# sourceMappingURL=index.da04a2a2.js.map
---
+++ dist/presentacio.cf8962d8.js
@@ -0,0 +1,2 @@
+!function e(){var t=new Date,n=t.getHours(),o=t.getMinutes(),c=t.getSeconds(),i="AM";0==n&&(n=12),n>12&&(n-=12,i="PM");var l=(n=n<10?"0"+n:n)+":"+(o=o<10?"0"+o:o)+":"+(c=c<10?"0"+c:c)+" "+i;document.getElementById("MyClockDisplay").innerText=l,document.getElementById("MyClockDisplay").textContent=l,setTimeout(e,1e3)}();
+//# sourceMappingURL=presentacio.cf8962d8.js.map
